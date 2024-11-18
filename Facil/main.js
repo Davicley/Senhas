893943753aaac0bb6d641a515373f42d0076262e
Davicley <128057976+Davicley@users.noreply.github.com>
2024-11-18 11:38:31 -0300

    Delete Facil directory

--- Facil/main.js
+++
@@ -1,5 +0,0 @@
-const numeroSenha = document.querySelector('.Parametro-Senha-Texto');
-let tamanhoSenha = 5000; 
-numeroSenha .textContent = tamanhoSenha;
-const botoes = document.querySelector('.Parametro-Senha-Botoes');
-botoes[0].onclick = diminuirTamanho;
