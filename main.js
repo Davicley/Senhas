6a12ae23ffc5a2e3b34f38e42b24989974979839
Davicley <128057976+Davicley@users.noreply.github.com>
2024-11-25 11:33:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const numeroSenha = document.querySelector('.Parametro-Senha-Texto');
+let tamanhoSenha = 10; 
+numeroSenha.textContent = tamanhoSenha;
+const botoes = document.querySelectorAll('.Parametro-Senha-Botao');const CampoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = "QWERTYUIOPASDFGHJKLÇZXCVBNMÀÉÁ";
+
+
+
+botoes[0].onclick = aumentarTamanho;
+geraSenha();
+botoes[1].onclick = diminuirTamanho;
+
+function diminuirTamanho(){
+    if(tamanhoSenha>6){
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha;
+    }
+}
+
+function aumentarTamanho(){
+    if(tamanhoSenha<15){
+    tamanhoSenha = tamanhoSenha + 1;
+    numeroSenha.textContent = tamanhoSenha;
+    }
+} 
+    function geraSenha(){
+        for (let i=0; i<tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio); 
+        senha = senha + letrasMaiusculas [numeroAleatorio];
+
+    }
+}
